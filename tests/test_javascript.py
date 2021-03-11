fb662ae90a8c959eaac653c762dfdea22c24b33f
Timothée Mazzucotelli <pawamoy@pm.me>
2021-03-11 22:02:20 +0100

    refactor: Fix docs and tests

---
+++ tests/test_javascript.py
@@ -0,0 +1,8 @@
+"""Tests for the `cli` module."""
+
+from mkdocstrings.handlers import javascript
+
+
+def test_main():
+    """The Javascript can be imported once installed."""
+    assert javascript
